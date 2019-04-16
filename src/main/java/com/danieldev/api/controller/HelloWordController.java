274297b1ce24ad78e765438d1ecfad89ffcb436b
Daniel Lopes <viana.daniel02@gmail.com>
2019-04-16 11:30:14 -0300

    Hello World com thymeleaf

---
+++ src/main/java/com/danieldev/api/controller/HelloWordController.java
@@ -0,0 +1,22 @@
+package com.danieldev.api.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HelloWordController {
+
+//	@RequestMapping(method = RequestMethod.GET, path = "/")
+	
+	@GetMapping
+	public ModelAndView index(Model model) {
+		ModelAndView mv = new ModelAndView("/helloword");
+		
+		return mv;
+	}
+	
+}
