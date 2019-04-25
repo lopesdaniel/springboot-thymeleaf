10396d2852462c9e7a11dd5b6d74e301e4bcf1ff
Daniel Lopes <viana.daniel02@gmail.com>
2019-04-25 17:21:26 -0300

    Criar navbar utilizando fragments

--- src/main/java/com/danieldev/api/controller/HelloWordController.java
+++ src/main/java/com/danieldev/api/controller/HelloWordController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -21,6 +20,9 @@ public class HelloWordController {
 	
 	@GetMapping
 	public String index(Model model) {
+		
+		model.addAttribute("name", "Daniel");
+		
 		return "helloword";
 	}
 	
